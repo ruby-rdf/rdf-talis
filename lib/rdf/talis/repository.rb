cb59b7671674f85e8e8b2e363557032bdf21395b
Ben Lavender <blavender@gmail.com>
2010-04-08 11:07:27 +0200

    Use [] for RDF.Statement

--- lib/rdf/talis/repository.rb
+++ lib/rdf/talis/repository.rb
@@ -48,7 +48,7 @@ module RDF::Talis
 
     def reify(statement,node)
       statements = []
-      statements << RDF::Statement.new(node, RDF.type, RDF::URI('http://www.w3.org/1999/02/22-rdf-syntax-ns#Statement'))
+      statements << RDF::Statement.new(node, RDF.type, RDF[:Statement])
       statements << RDF::Statement.new(node, RDF.subject, statement.subject)
       statements << RDF::Statement.new(node, RDF.predicate, statement.predicate)
       statements << RDF::Statement.new(node, RDF.object, statement.object)
